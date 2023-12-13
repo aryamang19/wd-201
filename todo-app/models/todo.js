de72675b62ffabfd3b39086c7e6fee84e993d016
Aryaman G <153002414+aryamang19@users.noreply.github.com>
2023-12-14 01:25:09 +0530

    Create todo.js

---
+++ todo-app/models/todo.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const { Op, Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    static associate(models) {
+      Todo.belongsTo(models.User, {
+        foreignKey: 'userId'
+      });
+    }
+
+    static addTodo({ title, dueDate, userId }) {
+      return Todo.create({ title, dueDate, completed: false, userId });
+    }
+
+    static async overdue(userId) {
+      return await Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.lt]: new Date().toLocaleDateString("en-CA"),
+          },
+          userId,
+          completed: false
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static async dueLater(userId) {
+      return await Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: new Date(),
+          },
+          userId,
+          completed: false
+        }
+      });
+    }
+
+    static dueToday(userId) {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.eq]: new Date().toLocaleDateString("en-CA"),
+          },
+          userId,
+          completed: false
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static async completedItems(userId) {
+      return await Todo.findAll({
+        where: {
+          completed: true,
+          userId,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    setCompletionStatus() {
+      if (this.completed === true) {
+        return this.update({ completed: false });
+      } else if (this.completed === false) {
+        return this.update({ completed: true });
+      }
+    }
+  }
+
+  Todo.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: 5,
+        notNull: true
+      }
+    },
+    dueDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        notNull: true
+      }
+    },
+    completed: { type: DataTypes.BOOLEAN }
+  }, {
+    sequelize,
+    modelName: 'Todo',
+  });
+
+  return Todo;
+};
